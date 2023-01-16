b91517d8ba228e2d8129306e5fcf5cbd55a248b3
Toine MARLANGE <marlange.toine@protonmail.com>
2023-01-16 18:24:44 +0100

    start section menu , responsive to do and stylesheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,14 @@ links.forEach(link => {
         }, 400);
     })
 });
+
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card =>{
+    card.addEventListener('click', ()=>{
+        card.classList.toggle('cardactive');
+    });
+    
+    
+})
