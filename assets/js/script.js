0bdc9c239cbeb7999145101ae98e7895e6bf39db
Toine MARLANGE <marlange.toine@protonmail.com>
2023-02-15 18:24:36 +0400

    new javascript fonction for scroll animation, CSS modified, add section savoir faire, to do : animation of the menu and contact section

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,56 @@ window.addEventListener('scroll', (e) =>{
 });
 
 
+const allBoxes = document.querySelectorAll('.box');
+let boxPositions = [
+    {position: 'calc(50% - 270px)', threshold: 0.62},
+    {position: 'calc(50% + 270px)', threshold: 0.65},
+    {position: 'calc(50% - 270px)', threshold: 0.69},
+    {position: 'calc(50% + 270px)', threshold: 0.75},
+];
+
+function handleScroll() {
+    const scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    
+    allBoxes.forEach((box, index) => {
+        const boxPosition = boxPositions[index];
+        if (scrollValue > boxPosition.threshold) {
+            setBoxStyle(box, 1, boxPosition.position);
+        }
+    });
+}
+
+function setBoxStyle(box, opacity, position) {
+    box.style.opacity = opacity;
+    box.style.left = position;
+}
+
+if(window.matchMedia("(max-width : 1000px)").matches) {
+    boxPositions = [
+        {position: 'calc(50% - 60px)', threshold: 0.62},
+        {position: 'calc(50% - 60px)', threshold: 0.65},
+        {position: 'calc(50% - 60px)', threshold: 0.69},
+        {position: 'calc(50% - 60px)', threshold: 0.71},
+    ]
+ }
+
+ if(window.matchMedia("(max-width : 530px)").matches) {
+    boxPositions = [
+        {position: '50%', threshold: 0.67},
+        {position: '50%', threshold: 0.71},
+        {position: '50%', threshold: 0.75},
+        {position: '50%', threshold: 0.79},
+    ]
+ }
+ 
+
+window.addEventListener('scroll', handleScroll);
+
+
+
 window.addEventListener("scroll", (e) => {
-
-let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight; // multiplier par 100 si % désiré    
-
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight; // multiplier par 100 si % désiré    
+    const allBoxes = document.querySelectorAll('.box');
 //text-accueil
     if(scrollValue > 0.25 ){
         actext.style.opacity = 1;
@@ -23,12 +69,12 @@ let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHe
     }
 
 // img accueil
-    if(scrollValue > 0.30){
+    if(scrollValue > 0.28){
         acimg.style.opacity = 1;
         acimg.style.transform = 'none';
     }
-    
 });
+
 
 // mobile menu disparait onclick
 const links = document.querySelectorAll(".nav-link");
@@ -56,3 +102,23 @@ cards.forEach(card =>{
         }  
     });
 })
+
+
+// SAVOIR FAIRE APPARARITION 
+
+// const allRonds = document.querySelectorAll('.rond');
+// const allBoxes = document.querySelectorAll ('.box');
+
+// const controller = new ScrollMagic.Controller()
+
+// allBoxes.forEach(box => {
+
+//     for(i = 0; i < allRonds.length; i++){
+
+//         if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim'))
+
+//     }
+// })
+
+// const y = window.scrollY;
+// console.log(y);
